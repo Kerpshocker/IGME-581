52184e3ae1e285b39892e410d691457bd845d997
maxpeabody <student@c2025m-19.main.ad.rit.edu>
2015-04-12 14:11:51 -0400

    Fixed merging issue
    Previous merging of “scrollable” branch with master branch made it so
    program wouldn’t compile; deleted source of the problem.

--- MeetEmUp/ProfileViewController.m
+++ MeetEmUp/ProfileViewController.m
@@ -19,10 +19,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-<<<<<<< HEAD
-=======
+
   //self.myScrollView.contentSize = CGRectMake(320, 1500);
->>>>>>> origin/master
 }
 
 - (void)didReceiveMemoryWarning {
