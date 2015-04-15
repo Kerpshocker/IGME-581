6e4b4a67ec7335d991c566e7ea109f1d06bb59ca
Kerpshocker <nate.kierpiec@gmail.com>
2015-04-15 18:02:39 -0400

    Added new db method
    saves data

--- MeetEmUp/Database.m
+++ MeetEmUp/Database.m
@@ -56,4 +56,39 @@ NSString *MEETEMUP_URL = @"http://people.rit.edu/njk3054/database/";
     [dataTask resume];
 }
 
+//save data based on table passed in. some will have query params (THAT WE PASS IN) others wont. php file takes care of those
+- (void)SaveData:(NSString *)tableName{
+    //NSURLSession is a class used to downlaod data via HTTP
+    //ephemeralSessionConfig means we don't need to cache anything
+    NSURLSessionConfiguration *config = [NSURLSessionConfiguration ephemeralSessionConfiguration];
+    
+    //create a new NSURLSession
+    _session = [NSURLSession sessionWithConfiguration:config];
+    
+    //show the activity indicator in upper left of screen
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    
+    //dynamically built url
+    NSMutableString *searchString = [NSMutableString string];
+    [searchString appendString:MEETEMUP_URL];
+    [searchString appendString:tableName]; //may have added query string parameters
+    
+    NSURL *url = [NSURL URLWithString:searchString];
+    
+    //this is a data task where we request a resource
+    NSURLSessionDataTask *dataTask = [_session dataTaskWithURL:url
+     completionHandler:^(NSData *data, NSURLResponse *response, NSError *error){
+         //NSLog(@"data=%@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+         if(!error){
+             NSHTTPURLResponse *httpResp = (NSHTTPURLResponse*)response;
+             //HTTP status code 200 is ok
+             if(httpResp.statusCode == 200){
+                 NSLog(@"We saved something?");
+             }
+         }
+     }];
+    //starts or resumes the data task
+    [dataTask resume];
+}
+
 @end
