44c2b22d109bb4554bbed220c6fc7a316072f9fd
Kerpshocker <njk3054@GDD-64.main.ad.rit.edu>
2015-04-15 17:14:40 -0400

    Added php file to save users
    can save users to the database, need to pass in 2 query params. Username
    and Password.

--- database/loadDB.php
+++ database/loadDB.php
@@ -22,10 +22,10 @@
 	
 	//this will be used to filter out the bullshit entered by users from query params
 	function sanitize_string($str) {
-		$str = urldecode($str); // %20 to space, %7E to ~, etc...
-		$str = trim($str); // get rid of leading and trailing spaces
-		$str = strip_tags($str); // get rid of HTML and PHP tags
-		$str = addslashes($str); // add backslashes to (i.e. escape) quotes
+		$str = urldecode($str); 	// %20 to space, %7E to ~, etc...
+		$str = trim($str); 			// get rid of leading and trailing spaces
+		$str = strip_tags($str); 	// get rid of HTML and PHP tags
+		$str = addslashes($str); 	// add backslashes to (i.e. escape) quotes
 		return $str;
 	}
 ?>
